9bc24db735b24d6c1cd12f215fd4fbe4cc37fb7d
Diogo Melo <diogro@gmail.com>
2020-02-10 15:12:36 -0300

    remove evolqg dependency

--- deploy_all.R
+++ deploy_all.R
@@ -1,7 +1,7 @@
 if(!require(rsconnect)){install.packages("rsconnect"); library(rsconnect)}
 rsconnect::setAccountInfo(name='curso-genevol', 
                           token='95346A5518D36EC95824A1AC86E965D1', 
-                          secret='secret_key')
+                          secret='bw8GbsXSqQO7EbTRgYtDkCaVwWLfFP8CU4IhX9Vc')
 
 # Deploy
 if(!require(here)){install.packages("here"); library(here)}
