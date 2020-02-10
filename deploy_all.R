4668af8c1bdea90a827c8316092570e2e1a1c626
Diogo Melo <diogro@gmail.com>
2020-02-10 15:16:50 -0300

    woops

--- deploy_all.R
+++ deploy_all.R
@@ -1,7 +1,7 @@
 if(!require(rsconnect)){install.packages("rsconnect"); library(rsconnect)}
-rsconnect::setAccountInfo(name='curso-genevol', 
-                          token='95346A5518D36EC95824A1AC86E965D1', 
-                          secret='bw8GbsXSqQO7EbTRgYtDkCaVwWLfFP8CU4IhX9Vc')
+rsconnect::setAccountInfo(name='curso-genevol',
+                          token='95346A5518D36EC95824A1AC86E965D1',
+                          secret='secret_key')
 
 # Deploy
 if(!require(here)){install.packages("here"); library(here)}
